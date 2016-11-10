446aeabfc1738d1f05405b4cca9114a66b40118b
usuario <usuario@192.168.1.56>
2016-11-10 09:42:17 +0100

    pequeña modificacion para ver como funciona github

--- src/mercadona/Mercadona.java
+++ src/mercadona/Mercadona.java
@@ -143,6 +143,10 @@ public class Mercadona {
     }
 
     public static void main(String[] args) {
+        
+        
+        
+        
 
         //rellenardatos();
        //mostarTodosLosProductos();
